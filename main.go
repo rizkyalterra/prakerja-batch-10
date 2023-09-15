facd976fa36e1ee23f821445e876d4b7cb5c829e
moryku <mohammadrizkyk@gmail.com>
2023-09-15 19:35:25 +0700

    config port

--- main.go
+++ main.go
@@ -1,29 +1,37 @@
 package main
 
 import (
+	"os"
 	"prakerja10/configs"
 	"prakerja10/routes"
 
-	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 )
 
 
 
 func main(){
-	loadEnv()
+	// loadEnv()
 	configs.InitDatabase()
 	e := echo.New()
 	routes.InitRoute(e)
-	e.Start(":8000")
+	e.Start(getPort() )
 }
 
-func loadEnv(){
-	err := godotenv.Load()
-	if err != nil {
-		panic("Failed load env file")
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return ":8000"
 	}
+	return ":" + port
 }
+
+// func loadEnv(){
+// 	err := godotenv.Load()
+// 	if err != nil {
+// 		panic("Failed load env file")
+// 	}
+// }
 
 
 
